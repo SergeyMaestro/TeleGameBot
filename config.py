78f5655f45c38bb8b4bd518ccc9ba41e3ce0d1fc
SergeyMaestro <52579829+SergeyMaestro@users.noreply.github.com>
2020-09-28 16:38:30 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,31 @@
+#Токен бота
+token = "1163266674:AAHVl2m9zQNwtnkpRtIgGjFyareVa8ExeGs"
+#ID админов
+admins_id = [1372848775]
+#Бонус
+welcome_bonus = 0.1
+#Название бота
+bot_name = "cubiccasinotestbot"
+#Номер киви
+qiwi_num = "+380933060143"
+#Апи
+qiwi_api = "e27bc2d8630a12990d743cafe30ef1dd"
+apibtc = ""
+#Помощь dice
+dice_help = "Помощь"
+#Игровая коммисиия
+games_comission = 5
+#Комиссия на вывод
+withdraw_comission = 2
+#Ссылка на чат
+chat_link = "https://t.me/joinchat/P0UIWhFDh4Q6qgckLOG05Q"
+#Ссылка на канал с отзывами
+votes_link = "https://t.me/joinchat/P0UIWhFDh4Q6qgckLOG05Q"
+#Правила бота
+rules_bot = "Правила бота"
+#Минимальная сумма ставки
+min_sum_bet = 10
+#Максимальная сумма ставки
+max_sum_bet = 15000
+#ID чатов для отправки уведомлений о играх
+chats_ids = [-323257066]
